2fb7175453d719b1b4cb9551372896dddaf7deb8
masood <masoodpk555@gmail.com>
2024-07-26 14:24:15 +0530

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Todo from './components/Todo'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+   <Todo />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Todo.css"
+function Todo() {
+  return ( 
+    <div className='container'>
+        <div className='app-text'>
+    <h1>TODO-APP</h1>
+    <h2>List of todos</h2>
+</div>
+    </div>
+  )
+}
+
+export default Todo
