ec5a3d88c93d5bd1012614ffb8ed231a230611eb
masood <masoodpk555@gmail.com>
2024-07-27 23:03:09 +0530

    styling input btn

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,16 @@ function Todo() {
     <div className='container'>
         <div className='app-text'>
     <h1>TODO-APP</h1>
+    <div className='list-todo'>
     <h2>List of todos</h2>
+    <input value="" type="text" role="input"></input>
+    <button type='submit'>Add</button>
+    </div>
+   
+
+  
+
+
 </div>
     </div>
   )
