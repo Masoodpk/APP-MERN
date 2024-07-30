3b45b442bddd69741e6a5469687239ffcc8797f2
masood <masoodpk555@gmail.com>
2024-07-30 11:46:20 +0530

    set input values

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Todo.css"
 function Todo() {
+    const [input,setInput] = useState('');
+    console.log(input, "input");
   return ( 
     <div className='container'>
         <div className='app-text'>
     <h1>TODO-APP</h1>
     <div className='list-todo'>
     <h2>List of todos</h2>
-    <input value="" type="text" role="input"></input>
+    <input value={input} onChange={(e)=>setInput(e.target.value)} type="text" role="input"></input>
     <button type='submit'>Add</button>
     </div>
    
