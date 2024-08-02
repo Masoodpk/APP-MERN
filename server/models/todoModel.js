c1a5cfdae3eb5047b2d451c778faedd5a668fdc0
masood <masoodpk555@gmail.com>
2024-08-02 15:22:15 +0530

    creating end points

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -1,6 +1,20 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
 
 const todoSchema = mongoose.Schema({
+text:{
+    type: String,
+    required: true
+},
+completed:{
+    type: Boolean,
+    required: true
+}
 
-    
-})
+
+
+},
+{timestamps:true}
+)
+
+export const Todo = mongoose.model("todo",todoSchema)
